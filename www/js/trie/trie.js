45815c37f5cb9d72b0e68e4fa6d51372e4764398
Ben Crulis <bencrulis@gmail.com>
2018-03-20 21:36:25 +0100

    added function to test word existence in trie

--- www/js/trie/trie.js
+++ www/js/trie/trie.js
@@ -1,34 +1,60 @@
-
-mkTrie = function(label, final, forest) {
-  return {
-    label: label,
-    final: final,
-    forest: forest
-  };
+var mkTrie = function(label, final, forest) {
+    return {
+        label: label,
+        final: final,
+        forest: forest
+    };
 };
 
-buildTrie = function(mots){
-  var arbre = mkTrie(null, false, []);
-  mots.forEach(function(mot){
-    var pointeur = arbre;
+var buildTrie = function(mots) {
+    var arbre = mkTrie(null, false, []);
+    mots.forEach(function(mot) {
+        var pointeur = arbre;
+        for (var i = 0; i < mot.length; i++) {
+            let ch = mot.charAt(i);
+
+            var temp = pointeur.forest.find(function(son) {
+                return son.label === ch;
+            });
+            if (temp == null) {
+                temp = mkTrie(ch, false, []);
+                pointeur.forest.push(temp);
+            }
+            pointeur = temp;
+
+            if (i === mot.length - 1) {
+                pointeur.final = true;
+            }
+        }
+    });
+    return arbre;
+};
+
+function findInForest(value, forest) {
+    forest.forEach(function(node) {
+        if (node.label === value) {
+            return node;
+        }
+    });
+    return null;
+}
+
+function isValidInTrie(trie, word) {
+    var p = trie;
     for (var i = 0; i < mot.length; i++) {
-      let ch = mot.charAt(i);
+        let ch = word.charAt(i);
+        son = findInForest(ch, p.forest);
+        if (son != null) {
+            if (word.length - 1 === i) {
+                return son.final;
+            } else {
+                p = son;
+            }
+        } else {
+            return false;
+        }
 
-      var temp = pointeur.forest.find(function(son) {
-        return son.label === ch;
-      });
-      if (temp == null){
-        temp = mkTrie(ch, false, []);
-        pointeur.forest.push(temp);
-      }
-      pointeur = temp;
-
-      if (i === mot.length - 1){
-        pointeur.final = true;
-      }
     }
-  });
-  return arbre;
-};
+}
 
-var qqesMots = ["banane","baracuda", "baraque","chip", "chopstick"];
+var qqesMots = ["banane", "baracuda", "baraque", "chip", "chopstick"];
