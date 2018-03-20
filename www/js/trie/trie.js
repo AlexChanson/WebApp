a667693f65cadd5158511907f7f8d622bc948573
Ben Crulis <21504399t@univ-tours.local>
2018-03-20 14:01:49 +0100

    making trie structure and builder

---
+++ www/js/trie/trie.js
@@ -0,0 +1,36 @@
+
+mkTrie = function(label, final, forest) {
+  return {
+    label: label,
+    final: final,
+    forest: forest
+  };
+}
+
+buildTrie = function(mots){
+  var arbre = null;
+  mots.foreach(function(mot){
+    var pointeur = arbre;
+    for (var i = 0; i < str.length; i++) {
+      let ch = str.charAt(i);
+      if (pointeur == null){
+        arbre = mkTrie(ch, false, []);
+        pointeur = arbre;
+      }
+      else {
+        let temp = pointer.forest.find(function(son){
+          return son.label === ch;
+        });
+        if (temp == null){
+          temp = mkTrie(ch, false, []);
+          pointeur.forest.push(temp);
+
+        }
+        pointeur = temp;
+      }
+      if (i === mot.length - 1){
+        pointeur.final = true;
+      }
+    }
+  });
+}
