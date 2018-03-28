e8b995d2c5b39c59151771d83030e8e31999cb3a
Clément Derouet <3065167a@opayq.com>
2018-03-28 16:46:09 +0200

    Filtre population

--- js/app.js
+++ js/app.js
@@ -1,14 +1,33 @@
+const populationSlider = new Slider('#slider-population', {
+    min: 0,
+    max: 1500000,
+    value: [0, 1500000],
+    scale: "logarithmic",
+    range: true
+});
+
 class FilterMenuViewModel {
     constructor() {
         this.toggleClose = ko.observable(false);
+        this.populationMin = ko.observable(0);
+        this.populationMax = ko.observable(1500000);
+        this.submitText = ko.observable("Afficher les données");
+
+        populationSlider.on("change", () => {
+            const range = populationSlider.getValue();
+
+            this.populationMin(range[0]);
+            this.populationMax(range[1]);
+        });
     }
 
-    closeMenu() {
+    submitFilters() {
         this.toggleClose(true);
     }
 
-    openMenu() {
+    openFilters() {
         this.toggleClose(false);
+        this.submitText("Mettre à jour les données");
     }
 }
 
--- www/js/app.js
+++ www/js/app.js
@@ -1,25 +1,46 @@
-'use strict';
+"use strict";
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
+var populationSlider = new Slider('#slider-population', {
+    min: 0,
+    max: 1500000,
+    value: [0, 1500000],
+    scale: "logarithmic",
+    range: true
+});
+
 var FilterMenuViewModel = function () {
     function FilterMenuViewModel() {
+        var _this = this;
+
         _classCallCheck(this, FilterMenuViewModel);
 
         this.toggleClose = ko.observable(false);
+        this.populationMin = ko.observable(0);
+        this.populationMax = ko.observable(1500000);
+        this.submitText = ko.observable("Afficher les données");
+
+        populationSlider.on("change", function () {
+            var range = populationSlider.getValue();
+
+            _this.populationMin(range[0]);
+            _this.populationMax(range[1]);
+        });
     }
 
     _createClass(FilterMenuViewModel, [{
-        key: 'closeMenu',
-        value: function closeMenu() {
+        key: "submitFilters",
+        value: function submitFilters() {
             this.toggleClose(true);
         }
     }, {
-        key: 'openMenu',
-        value: function openMenu() {
+        key: "openFilters",
+        value: function openFilters() {
             this.toggleClose(false);
+            this.submitText("Mettre à jour les données");
         }
     }]);
 
@@ -34,8 +55,7 @@ Highcharts.mapChart('map', {
         backgroundColor: "#495057"
     },
     series: [{
-        showInLegend: false,
-        color: "#000"
+        showInLegend: false
     }, {
         name: 'Separators',
         type: 'mapline',
