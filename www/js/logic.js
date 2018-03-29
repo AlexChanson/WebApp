fa631e7dcddae1e8ee16a4d7b48fc1946ee5c7f6
Alexandre Chanson <a.chanson.tours@gmail.com>
2018-03-29 17:31:46 +0200

    Switch to new webb application

---
+++ www/js/logic.js
@@ -0,0 +1,239 @@
+const server_domain = "elsa.alexc.ovh";
+const pages = ["acceuil", "login", "create", "app"];
+const stylesheets = ["css/clem.css", "css/clem.css", "css/clem.css",
+    "css/app.css"
+];
+const onLoads = [onLoadAcceuil, onLoadLogin, onLoadCreate, onLoadApp];
+const onLeaves = [onLeaveAcceuil, onLeaveLogin, onLeaveCreate, onLeaveApp];
+
+let page_state = 'acceuil';
+let api_key = null;
+let nav_state = true;
+
+let cities = [];
+let departements = [];
+let regions = [];
+
+const getRegionsJSON = '{"type":"getRegions"}';
+const getCityNamesJSON = '{"type":"getCityNames"}';
+const getDepartementsJSON = '{"type":"getDepartements"}';
+
+
+class VirtualPage {
+    constructor(name, stylesheet, onL, onD) {
+        this.name = name;
+        this.stylesheet = stylesheet;
+        this.onL = onL;
+        this.onD = onD;
+    }
+
+    load(){
+        document.getElementById(this.name).hidden = false;
+        document.getElementById('dynamic_css').setAttribute('href', this.stylesheet);
+        this.onL();
+    }
+
+    unload(){
+        document.getElementById(this.name).hidden = true;
+        this.onD();
+    }
+}
+
+class VirtualPageManager{
+    constructor(defaultPage){
+        this.pages = new Map();
+        this.pages.set(defaultPage.name, defaultPage);
+        defaultPage.load();
+    }
+
+    registerPage(vPage){
+        this.pages.set(vPage.name, vPage);
+    }
+
+    swapTo(name){
+        const target = this.pages.get(name);
+        this.pages.forEach(function (key, value) {
+            if(value.name !== target.name)
+                value.unload();
+        });
+        target.load();
+    }
+}
+
+
+onLoad();
+
+function onLoad() {
+    const user = localStorage.getItem("user");
+    console.log(user);
+    if (user != null) {
+        api_key = JSON.parse(user).api;
+        swapTo('app');
+        onConnect();
+    }
+}
+
+function onLoadAcceuil() {
+
+}
+
+function onLoadLogin() {
+
+}
+
+function onLoadCreate() {
+
+}
+
+function onLoadApp() {
+    document.getElementById("page_body").classList.add("bg-light");
+}
+
+function onLeaveAcceuil() {
+
+}
+
+function onLeaveLogin() {
+
+}
+
+function onLeaveCreate() {
+
+}
+
+function onLeaveApp() {
+    document.getElementById("page_body").classList.remove("bg-light");
+}
+
+function onConnect() {
+    elsaRequest(getRegionsJSON, resp => {
+        regions = JSON.parse(resp);
+    });
+    elsaRequest(getDepartementsJSON, resp => {
+        departements = JSON.parse(resp);
+    });
+    elsaRequest(getCityNamesJSON, resp => {
+        cities = JSON.parse(resp);
+
+        console.log("cities loading in browser...");
+        let datalistCommuneA = document.getElementById("listCommunes");
+        let child = null;
+        for (jsonCity of cities) {
+            const name = jsonCity.nom;
+            if (name) {
+                child = document.createElement("option");
+                child.setAttribute("value", name);
+                datalistCommuneA.appendChild(child);
+            }
+        }
+        console.log(cities.length.toString() + " cities loaded.");
+
+    });
+
+}
+
+
+function swapTo(nom) {
+    for (let i = 0; i < pages.length; i++) {
+        if (pages[i] !== nom) {
+            document.getElementById(pages[i]).hidden = true;
+            onLeaves[i]();
+        } else {
+            document.getElementById(pages[i]).hidden = false;
+            document.getElementById('dynamic_css').setAttribute('href',
+                stylesheets[i]);
+            page_state = nom;
+            onLoads[i]();
+        }
+    }
+}
+
+function toggleNav() {
+    if (nav_state === true) {
+        document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("app_openNav").style.color = '#555';
+        nav_state = false;
+    } else {
+        document.getElementById("mySidenav").style.width = "20%";
+        document.getElementById("app_openNav").style.color = '#ccc';
+        nav_state = true;
+    }
+}
+
+
+function elsa_Connection(email, password) {
+    let xhr = new XMLHttpRequest();
+    const url = 'http://elsa.alexc.ovh/connect';
+    const body = '{"email":"' + email + '", "password":"' + password + '"}';
+    xhr.open('POST', url);
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+            let ret = JSON.parse(xhr.responseText);
+            if (ret.hasOwnProperty('api_key')) {
+                api_key = ret.api_key;
+                swapTo('app');
+                if (document.getElementById("connectionPersistent").checked)
+                    localStorage.setItem("user", JSON.stringify({
+                        email: email,
+                        password: password,
+                        api: api_key
+                    }));
+                onConnect();
+            }
+        }
+    };
+    xhr.send(body);
+}
+
+function login_Connecter() {
+    let mail = document.getElementById('login_adresseEmail').value;
+    let pass = document.getElementById('login_motDePasse').value;
+    elsa_Connection(mail, pass);
+}
+
+function create_Submit() {
+    const mdp = helpers_get('create_motDePasse');
+    const mdp2 = helpers_get('create_confirmerMotDePasse');
+    const user = {
+        email: helpers_get('create_adresseEmail'),
+        nom: helpers_get('create_nom'),
+        prenom: helpers_get('create_prenom'),
+        password: mdp
+    };
+
+    if (mdp === mdp2) {
+        let json = JSON.stringify(user);
+        let xhr = new XMLHttpRequest();
+
+        function callback() {
+            if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+                swapTo('app');
+                console.log(xhr.responseText);
+            }
+        }
+        const url = 'http://' + server_domain + '/connect';
+        xhr.open('POST', url);
+        xhr.onreadystatechange = callback;
+        xhr.send(json);
+    } else {
+        document.getElementById('create_mdpZone2').style.color = "red";
+    }
+}
+
+function helpers_get(id) {
+    return document.getElementById(id).value;
+}
+
+function elsaRequest(body, callback) {
+    const url = "http://" + server_domain + "/api?key=" + api_key;
+    let xhr = new XMLHttpRequest();
+
+    function internCallback() {
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+            callback(xhr.responseText);
+        }
+    }
+    xhr.open('POST', url);
+    xhr.onreadystatechange = internCallback;
+    xhr.send(body);
+}
---
+++ www/js/trie.js
@@ -0,0 +1,101 @@
+var mkTrie = function(label, final, forest) {
+    return {
+        label: label,
+        final: final,
+        forest: forest
+    };
+};
+
+var buildTrie = function(mots) {
+    var arbre = mkTrie(null, false, []);
+    mots.forEach(function(mot) {
+        var pointeur = arbre;
+        for (var i = 0; i < mot.length; i++) {
+            let ch = mot.charAt(i);
+
+            var temp = pointeur.forest.find(function(son) {
+                return son.label === ch;
+            });
+            if (temp == null) {
+                temp = mkTrie(ch, false, []);
+                pointeur.forest.push(temp);
+            }
+            pointeur = temp;
+
+            if (i === mot.length - 1) {
+                pointeur.final = true;
+            }
+        }
+    });
+    return arbre;
+};
+
+function findInForest(value, forest) {
+    for (let node of forest) {
+        if (node.label === value) {
+            return node;
+        }
+    }
+    return null;
+}
+
+function isValidInTrie(trie, word) {
+    var p = trie;
+    for (var i = 0; i < word.length; i++) {
+        let ch = word.charAt(i);
+        var son = findInForest(ch, p.forest);
+        console.log(ch);
+        if (son != null) {
+            if (word.length - 1 === i) {
+                return son.final;
+            } else {
+                p = son;
+            }
+        } else {
+            return false;
+        }
+    }
+    return false;
+}
+
+function findNodeFromWord(trie, word){
+    var p = trie;
+    for (var i = 0; i < word.length; i++) {
+        let ch = word.charAt(i);
+        var son = findInForest(ch, p.forest);
+        if (son != null) {
+            if (word.length - 1 === i) {
+                return son;
+            } else {
+                p = son;
+            }
+        } else {
+            return null;
+        }
+    }
+    return false;
+}
+
+function collapseTrie(f, trie){
+    return f(trie.final, trie.label, [].concat.apply([], trie.forest.map(x => collapseTrie(f, x) ) ) );
+}
+
+function formWords(final, label, wordsEnds){
+    let temp = wordsEnds.map(x => label+x);
+    if (final){
+        temp.push(label);
+        return temp;
+    }
+    else {
+        return temp;
+    }
+}
+
+function findPossibleEnds(trie, word){
+    var son = findNodeFromWord(trie, word);
+    return collapseTrie(formWords, son).map(x => word + x.substr(1));
+}
+
+var qqesMots = ["banane", "baracuda", "baraque", "chip", "chopstick"];
+
+var testTrie = buildTrie(qqesMots);
