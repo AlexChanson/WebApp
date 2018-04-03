3f4656e1a577d7eca662d173886db85ddedf6d5c
Alexandre Chanson <a.chanson.tours@gmail.com>
2018-04-03 10:50:19 +0200

    returns server info from csv loading

--- www/js/logic.js
+++ www/js/logic.js
@@ -631,8 +631,9 @@ function loadCsv() {
                 "csv": freader.result.replace(/[\r| ]/g, "").split(
                     "\n")
             }), res => {
-                // TODO
-                alert("Chargement du Fichier Reussi !");
+                const ans = JSON.parse(res);
+                if (ans.hasOwnProperty('status') && ans.status === "done")
+                    alert("Chargement du Fichier Reussi !\n" + ans.errors + " erreurs d'Inserion.");
             });
         };
     }
